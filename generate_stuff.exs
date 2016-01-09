ad342e37ebcb1860993768afbb73b3e8fa9f5e73
Roman Heinrich <roman.heinrich@gmail.com>
2016-01-09 15:45:50 +0100

    generate stuff script

---
+++ generate_stuff.exs
@@ -0,0 +1,7 @@
+## git clean -f -d -n -> see what changes
+## git clena -f -d -> !! removes untracked files
+File.cd "apps/back"
+range = 1..2 |> Enum.map(&(&1))
+template = fn(x)-> "mix phoenix.gen.html  Monster#{x} monsters#{x} name:string description:string" end
+doerfn = fn(x)-> :os.cmd(x |> to_char_list) |> IO.puts end
+range |> Enum.map(fn(x) -> doerfn.(template.(x)) end)
