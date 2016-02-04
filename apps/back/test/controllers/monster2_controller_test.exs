ec21ca5e9ed17f941a37bc6020f3e355432cc128
Roman Heinrich <roman.heinrich@gmail.com>
2016-02-04 17:13:27 +0100

    fix with timing

--- apps/back/test/controllers/monster2_controller_test.exs
+++ apps/back/test/controllers/monster2_controller_test.exs
@@ -12,7 +12,7 @@ defmodule Back.Monster2ControllerTest do
 
   test "renders form for new resources", %{conn: conn} do
     conn = get conn, monster2_path(conn, :new)
-    assert html_response(conn, 200) =~ "New monster24"
+    assert html_response(conn, 200) =~ "New monster2"
   end
 
   test "creates resource and redirects when data is valid", %{conn: conn} do
